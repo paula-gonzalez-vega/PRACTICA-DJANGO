f1d79437588b4dc51f258eb29324087d4cb694c7
Paulagove <paulagon27@gmail.com>
2025-12-01 17:21:23 +0100

    Subida aplicación a GitHub

---
+++ relecloud/models.py
@@ -0,0 +1,63 @@
+from django.db import models
+
+# Modelo de destino
+class Destination(models.Model):
+    name = models.CharField(
+        unique=True,
+        null=False,
+        blank=False,
+        max_length=50
+    )
+    description = models.TextField(
+        max_length=2000,
+        null=False,
+        blank=False
+    )
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('destination_detail', kwargs={'pk': self.pk})
+
+
+# Modelo de crucero
+class Cruise(models.Model):
+    name = models.CharField(
+        unique=True,
+        null=False,
+        blank=False,
+        max_length=50
+    )
+    description = models.TextField(
+        max_length=2000,
+        null=False,
+        blank=False
+    )
+    destinations = models.ManyToManyField(
+        Destination,
+        related_name='cruises'
+    )
+
+    def __str__(self):
+        return self.name
+
+
+# Modelo de solicitud de información
+class InfoRequest(models.Model):
+    name = models.CharField(
+        null=False,
+        blank=False,
+        max_length=50
+    )
+    email = models.EmailField()
+    notes = models.TextField(
+        max_length=2000,
+        null=False,
+        blank=False
+    )
+    cruise = models.ForeignKey(
+        Cruise,
+        on_delete=models.PROTECT
+    )
+
